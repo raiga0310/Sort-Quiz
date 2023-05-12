b8b60e2f38b3c184972c46717412b79135471398
ライガー <44018535+raiga0310@users.noreply.github.com>
2023-05-12 08:53:57 +0000

    refactor: 採点ロジックの実装のために生成・採点部分をPlayScreenに移動

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,43 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import SortableCard from './SortableCard'
-import { getLanguageInfo } from '../data/data';
 
-const generateCards = (Array) => {
-  console.log('generateCards');
-  console.log(Array);
-  const quizes = getLanguageInfo(Array);
-  console.log(quizes);
-  const cards = [];
-  for (let i = 0; i < quizes.length; i++) {
-    cards.push({
-      id: i, // もしくは年を入れる
-      front: quizes[i][2],
-      back: {title: quizes[i][2], description: quizes[i][1]},
-      corner: quizes[i][3],
-    })
-  }
-  if(cards.length < 5) {
-    console.log('uooooooooooooooooooooooooooooooooooooooooooooooooo');
-  }
-  return cards
-}
-
-const CardList = ({ quizKeyArray }) => {
-  const [cards, setCards] = useState(generateCards(quizKeyArray));
-
-  // quizKeyArrayが変更されたときに新しいカード列を生成
-  useEffect(() => {
-    setCards(generateCards(quizKeyArray));
-  }, [quizKeyArray]);
-
-  const moveCard = (fromIndex, toIndex) => {
-    const newCards = [...cards]
-    const cardToMove = newCards[fromIndex]
-    newCards.splice(fromIndex, 1)
-    newCards.splice(toIndex, 0, cardToMove)
-    setCards(newCards)
-  }
-
+const CardList = ({ cards, moveCard }) => {
   return (
     <div className="card-list">
       {cards.map((card, index) => (
@@ -49,7 +13,7 @@ const CardList = ({ quizKeyArray }) => {
         />
       ))}
     </div>
-  )
+  );
 }
 
 export default CardList
--- src/components/PlayScreen.js
+++ src/components/PlayScreen.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './CardList';  // カードリストコンポーネントをインポート
-import '../data/data';
+import { getLanguageInfo } from '../data/data';
 import '../App.css';
 import './PlayScreen.css';
 
@@ -14,16 +14,90 @@ function getRandomArray(n) {
   }
   return array;
 }
+const generateCards = (Array) => {
+  const quizes = getLanguageInfo(Array);
+  const years = quizes.map((quiz) => parseInt(quiz[0]));
+  const descriptions = quizes.map((quiz) => quiz[1]);
+  const titles = quizes.map((quiz) => quiz[2]);
+  const extensions = quizes.map((quiz) => quiz[3]);
+  
+  // 年とインデックスをペアにして降順に並べ替え
+  const sortedIndexes = years.map((year, index) => ({year, index})).sort((a, b) => b.year - a.year);
 
-function PlayScreen({ onAnswer }) {
+  // 並べ替えた後のインデックスを取得
+  const correctOrder = sortedIndexes.map(item => item.index);
+
+  const cards = [];
+  for (let i = 0; i < quizes.length; i++) {
+    cards.push({
+      id: years[i], 
+      front: titles[i],
+      back: {title: titles[i], description: descriptions[i]},
+      corner: extensions[i],
+      order: correctOrder.indexOf(i),  // カードの正解の順序を追加
+    })
+  }
+  return cards;
+}
+
+function kendallsTau(arr1, arr2) {
+  console.log('arr1:', arr1);
+  console.log('arr2:', arr2);
+
+  let concordant = 0;
+  let discordant = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = i+1; j < arr2.length; j++) {
+      let arr1_i = arr1[i], arr1_j = arr1[j];
+      let arr2_i = arr2.findIndex(val => val === arr1_i);
+      let arr2_j = arr2.findIndex(val => val === arr1_j);
+      if ((arr2_i - arr2_j) * (i - j) > 0) {
+        concordant += 1;
+      } else if ((arr2_i - arr2_j) * (i - j) < 0) {
+        discordant += 1;
+      }
+    }
+  }
+  console.log('concordant:', concordant);
+  console.log('discordant:', discordant);
+
+  const tau = Number(((concordant - discordant) / (concordant + discordant)).toFixed(4));
+
+  return tau; 
+}
+
+
+function PlayScreen({ player, onEnd }) {
   const [quizKeyArray, setQuizKeyArray] = useState(getRandomArray(5));
+  const [cards, setCards] = useState(generateCards(quizKeyArray));
+
+  // quizKeyArrayが変更されたときに新しいカード列を生成
+  useEffect(() => {
+    setCards(generateCards(quizKeyArray));
+  }, [quizKeyArray]);
+
+  const moveCard = (fromIndex, toIndex) => {
+    const newCards = [...cards]
+    const cardToMove = newCards[fromIndex]
+    newCards.splice(fromIndex, 1)
+    newCards.splice(toIndex, 0, cardToMove)
+    setCards(newCards)
+  }
 
   const handleAnswer = () => {
-    // ユーザーの回答を採点する処理
-    // ここでは仮に常に正解とする
-    const isCorrect = true;
-    onAnswer(isCorrect);
+    // ユーザーのカードの順序と正解の順序を取得
+    const userOrder = cards.map(card => card.id);
+    
+    // キーからリリース年を取得して降順にソート
+    const correctOrder = getLanguageInfo(quizKeyArray).map(info => parseInt(info[0])).sort((a, b) => b - a);
+  
+    // ケンドールの相関係数を計算
+    const tau = kendallsTau(userOrder, correctOrder).toFixed(4);
+  
+    console.log(tau);
+    onEnd(tau);
   };
+  
 
   const handleRefresh = () => {
     // Refreshボタンが押されたときに新しい乱数配列を生成してstateを更新
@@ -36,7 +110,7 @@ function PlayScreen({ onAnswer }) {
       <p>リリース年の降順に並び替えなさい｡</p>
       { 
       <CardList
-        quizKeyArray={quizKeyArray}
+        cards={cards} moveCard={moveCard}
       /> 
       }
       <div className='play-screen-buttons'>
